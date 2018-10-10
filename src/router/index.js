b7634015f93913d544007d6beef03e0926e7c2c1
zkl <kaili.zheng@91wutong.com>
2018-10-10 15:29:04 +0800

    router首页更换

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,16 @@
 module.exports = [
   {
-    path: 'pages/home/index',
-    config: {
-      navigationBarTitleText: 'home页面',
-    }
-  },
-  {
     path: 'pages/index/index',
     config: {
       navigationBarTitleText: '首页',
     }
   },
-
+  {
+    path: 'pages/home/index',
+    config: {
+      navigationBarTitleText: 'home页面',
+    }
+  },
   {
     path: 'pages/counter/index',
     config: {
